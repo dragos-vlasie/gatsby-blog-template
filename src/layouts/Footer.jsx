0baefde4de55510af5968b8a9a795379e450bac1
dragos-vlasie <dragos.vlasie@thisplace.com>
2019-11-01 14:47:08 +0000

    Update Footer.jsx

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -25,8 +25,8 @@ const Footer = () => (
   <Wrapper>
     <Text>
       <span>
-        Gatsby Tutorial Starter -{' '}
-        <a href="https://justinformentin.com">Justin Formentin</a>
+        Gatsby Template-{' '}
+        <a href="/">Dragos Vlasie</a>
       </span>
     </Text>
   </Wrapper>
