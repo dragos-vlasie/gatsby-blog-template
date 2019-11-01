98285061de0e412b43da0f535eeae7cb0ee575c3
dragos-vlasie <dragos.vlasie@thisplace.com>
2019-11-01 14:36:31 +0000

    Initial commit

---
+++ config/site.js
@@ -0,0 +1,16 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+  title: 'Gatsby blog template', // Navigation and Site Title
+  titleAlt: 'Gatsby blog template', // Title for JSONLD
+  description: 'A Gatsby blog Template',
+  siteLanguage: 'en', // Language Tag on <html> element
+  logo: 'static/logo/logo.png', // Used for SEO
+  banner: 'static/logo/banner.png',
+  // JSONLD / Manifest
+  favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
+  shortName: 'GatsbyTem', // shortname for manifest. MUST be shorter than 12 characters
+  author: 'Dragos', // Author for schemaORGJSONLD
+  themeColor: '#3e7bf2',
+  backgroundColor: '#d3e0ff',
+  twitter: '@VlasiDragos', // Twitter Username
+};
---
+++ src/components/Header.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import Img from 'gatsby-image';
+import PropTypes from 'prop-types';
+import { useSpring, config, animated } from 'react-spring';
+import Wave from '../elements/Wave';
+
+const Wrapper = styled.header`
+  background: ${props => props.theme.gradient.rightToLeft};
+  height: ${props => (props.big ? '650px' : '450px')};
+  @media (max-width: ${props => props.theme.breakpoints.m}) {
+    height: ${props => (props.big ? '600px' : '400px')};
+  }
+  @media (max-width: ${props => props.theme.breakpoints.s}) {
+    height: ${props => (props.big ? '500px' : '325px')};
+  }
+  position: relative;
+  overflow: hidden;
+`;
+
+const Text = styled.div`
+  color: ${props => props.theme.colors.white.base};
+  z-index: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  width: 100%;
+  max-width: ${props => props.theme.layout.base};
+  padding: 0 2rem;
+  margin-bottom: 3rem;
+  align-items: center;
+`;
+
+const Subtitle = styled(animated.p)`
+  max-width: 650px;
+  color: ${props => props.theme.colors.white.blue};
+`
+
+const Header = ({ children, title, date, cover, big }) => {
+  const titleProps = useSpring({
+    from: { opacity: 0, transform: 'translate3d(0, -30px, 0)' },
+    to: { opacity: 1, transform: 'translate3d(0, 0, 0)' },
+  });
+  const subProps = useSpring({
+    config: config.slow,
+    delay: 400,
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+  });
+  return (
+    <Wrapper big={big}>
+      <Img fluid={cover || {} || [] || ''} />
+      <Text>
+        <animated.h1 data-testid="header-title" style={titleProps}>
+          {title}
+        </animated.h1>
+        <animated.h3 data-testid="header-title" style={subProps}>
+          {date}
+        </animated.h3>
+
+        {children && <Subtitle style={subProps}>{children}</Subtitle>}
+      </Text>
+      <Wave />
+    </Wrapper>
+  );
+};
+
+export default Header;
+
+Header.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.bool]),
+  cover: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  date: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+  title: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object,
+    PropTypes.bool,
+  ]),
+};
+
+Header.defaultProps = {
+  children: false,
+  cover: false,
+  date: false,
+  title: false,
+  big: false,
+};
