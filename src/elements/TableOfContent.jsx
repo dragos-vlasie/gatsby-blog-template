85b45db516fd658fb9b8e1c904c0aa0d24fcd696
Dragos Vlasie <37800900+dragos-vlasie@users.noreply.github.com>
2020-07-19 14:05:09 +0700

    put styles outside of the component

--- src/elements/TableOfContent.jsx
+++ src/elements/TableOfContent.jsx
@@ -2,83 +2,83 @@ import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
 
-export const TableOfContent = ({ input }) => {
-  const Wrapper = styled.div`
-    background: white;
-    padding: 25px;
-    max-width: 50rem;
+const Wrapper = styled.div`
+  background: white;
+  padding: 25px;
+  max-width: 50rem;
+  transition: opacity 0.5s ease 0s;
+  @media (max-width: ${props => props.theme.breakpoints.s}) {
+    max-width: 95%;
+  }
+`;
+
+const FixedWrapper = styled.div`
+  display: none;
+  @media (max-width: ${props => props.theme.breakpoints.s}) {
+    display: block;
+
+    opacity: ${({ show }) => (show ? '1' : '0')};
     transition: opacity 0.5s ease 0s;
-    @media (max-width: ${props => props.theme.breakpoints.s}) {
-      max-width: 95%;
-    }
-  `;
+    top: 0px;
+    left: 0px;
+    bottom: 0px;
+    right: 0px;
+    position: fixed;
+    background-color: rgb(255, 255, 255);
+    z-index: 2;
+    height: 100vh;
+    overflow-y: auto;
+    pointer-events: ${props => (props.show ? 'auto' : 'none')};
+  }
+`;
 
-  const FixedWrapper = styled.div`
-    display: none;
-    @media (max-width: ${props => props.theme.breakpoints.s}) {
-      display: block;
-      opacity: ${props => (props.show ? '1' : '0')};
-      transition: opacity 0.5s ease 0s;
-      top: 0px;
-      left: 0px;
-      bottom: 0px;
-      right: 0px;
-      position: fixed;
-      background-color: rgb(255, 255, 255);
-      z-index: 2;
-      height: 100vh;
-      overflow-y: auto;
-      pointer-events: ${props => (props.show ? 'auto' : 'none')};
-    }
-  `;
-
-  const Button = styled.div`
-    display: none;
-    @media (max-width: ${props => props.theme.breakpoints.s}) {
-      background-color: rgb(32, 35, 42);
-      bottom: 44px;
-      color: rgb(97, 218, 251);
-      display: block;
-      cursor: pointer;
-      position: fixed;
-      right: 20px;
-      z-index: 3;
-      box-shadow: rgba(0, 0, 0, 0.3) 0px 0px 20px;
-      border-radius: 50%;
-      border-width: 1px;
-      border-style: solid;
-      border-color: rgba(255, 255, 255, 0.1);
-      border-image: initial;
-    }
-  `;
-  const InnerButton = styled.div`
-    padding-left: 20px;
-    padding-right: 20px;
-    margin-left: auto;
-    margin-right: auto;
-  `;
-  const IconsWrapper = styled.div`
-    height: 60px;
-    display: flex;
-    align-items: flex-start;
-    overflow: hidden;
-  `;
-  const Icons = styled.div`
-    width: 20px;
-    height: 20px;
-    display: flex;
+const Button = styled.div`
+  display: none;
+  @media (max-width: ${props => props.theme.breakpoints.s}) {
+    background-color: rgb(32, 35, 42);
+    bottom: 44px;
     color: rgb(97, 218, 251);
-    align-self: center;
-    flex-direction: column;
-  `;
+    display: block;
+    cursor: pointer;
+    position: fixed;
+    right: 20px;
+    z-index: 3;
+    box-shadow: rgba(0, 0, 0, 0.3) 0px 0px 20px;
+    border-radius: 50%;
+    border-width: 1px;
+    border-style: solid;
+    border-color: rgba(255, 255, 255, 0.1);
+    border-image: initial;
+  }
+`;
+const InnerButton = styled.div`
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+const IconsWrapper = styled.div`
+  height: 60px;
+  display: flex;
+  align-items: flex-start;
+  overflow: hidden;
+`;
+const Icons = styled.div`
+  width: 20px;
+  height: 20px;
+  display: flex;
+  color: rgb(97, 218, 251);
+  align-self: center;
+  flex-direction: column;
+`;
 
-  const ArrowUp = styled.svg`
-    transform: translate(2px, -4px) rotate(180deg);
-  `;
-  const ArrowDown = styled.svg`
-    transform: translate(2px, 4px);
-  `;
-
+const ArrowUp = styled.svg`
+  transform: translate(2px, -4px) rotate(180deg);
+`;
+const ArrowDown = styled.svg`
+  transform: translate(2px, 4px);
+`;
+export const TableOfContent = ({ input }) => {
   const [simpleButtonState, toggleSimpleButtonState] = useState(false);
   const handleSimpleButtonClick = () => toggleSimpleButtonState(!simpleButtonState);
 
